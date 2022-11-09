ada35395769449073daf5c69b02c74c37fa13636
Brogy-alt <brogangys2@gmail.com>
2022-11-09 14:17:41 +0200

    yeah

---
+++ js/code.js
@@ -0,0 +1,75 @@
+// Array 
+// use [] for array
+
+// let students = [
+//     `Gift`, `Asandile` ,
+//     `Deno`, `Javier`, `Imraan`, `Meezie outta here`
+// ];
+// console.log(students);
+// // display gift on console
+// // pc's count from 0
+// // use [] to select in console
+// console.log(students[0]);
+// console.log(students[2]);
+// // last element
+// console.log(students[size-1]);
+// // second last element
+// console.log(students[size-2]);
+
+// Exercise 
+
+// let students = [
+//     'Gift', 'Asandile',
+//     'Deno', 'Javier',
+//     'Sakhe', 'Mkhuseli',
+//     'John', 'Thando', 'Imraan'
+// ];
+// console.log(students);
+// console.log (students[(students.length-1)/2]);
+
+// Comparison operator
+// let numb1 = 6;
+// let numb2 = 7;
+// console.log(numb1 > numb2);
+// console.log(numb1 <= numb2);
+// console.log(numb1 >= numb2);
+// console.log(numb1 != numb2);
+// console.log(numb1 == numb2);
+// console.log(numb1 === numb2);
+
+
+// logical operators
+// parseFloat allows decimals
+// parseInt doesnt allow decimals
+// && = and
+// || = or
+
+// let salary = parseFloat(prompt("Enter your salary:"));
+// let age = parseInt(prompt ("Enter your age:"));
+// let qualified = !(salary >= 5000);
+// (age > 17);
+// document.write(qualified)
+
+// Rational operations : in
+// let numbers = [9,7,4,3,15];
+// console.log(7 in numbers);
+
+// Increment (++) and  (--) decrement
+// // increment
+// let numb1 = 8;
+// console.log(++numb1);
+
+// decrement
+// let numb1 = 8;
+// console.log(--numb1);
+
+// Ternary operator
+// ?
+let age = 17;
+console.log(
+    age>17 ? "You are qualified" :
+    "You are not qualified")
+
+
+
+
